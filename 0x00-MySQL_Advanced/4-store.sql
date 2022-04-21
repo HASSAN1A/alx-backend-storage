78781d16d1c94ee0f1f7e548895c8b64eb0de7c5
HASSAN1A <okothhassanjuma@gmail.com>
2022-04-21 20:49:53 +0300

    create trigger

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order
+-- NEW & OLD are MySQL extensions to triggers
+-- enable to access columns in the rows affected by a trigger
+
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
